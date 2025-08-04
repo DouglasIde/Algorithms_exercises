ddd5f990041a9bd0bc623d5285e0622fbc1d6c91
DouglasIde <douglasymide@gmail.com>
2025-08-04 19:37:11 -0300

    Feat: Solving exercise 1 from Introduction to Algorithms

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+from Chapter_1.pesquisa_binaria import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    lista_nomes_telefone = [
+        {"nome": "Ana Flavia", "telefone": "(11) 98765-4321", "operadora": "Vivo"},
+        {"nome": "Douglas Ide", "telefone": "(21) 99123-4567", "operadora": "TIM"},
+        {"nome": "Fernanda Ribeiro", "telefone": "(31) 99876-1234", "operadora": "Claro"},
+        {"nome": "Thiago Vasques", "telefone": "(41) 99654-7890", "operadora": "Oi"},
+        {"nome": "Juliana Castro", "telefone": "(61) 98456-3210", "operadora": "Vivo"},
+        {"nome": "Marcos da Silva", "telefone": "(51) 99777-8888", "operadora": "TIM"},
+        {"nome": "Sabrina Moura", "telefone": "(71) 99333-2222", "operadora": "Claro"},
+        {"nome": "Eduardo Martins", "telefone": "(91) 99900-1100", "operadora": "Oi"},
+        {"nome": "Patrícia Nogueira", "telefone": "(85) 99888-6655", "operadora": "Claro"},
+        {"nome": "Rafael Antunes", "telefone": "(27) 99666-4433", "operadora": "Vivo"}
+    ]
+
+    nome = input("Digite o nome que deseja encontrar na lista telefonica")
+
+    # Tratar os dados para garantir que procure de forma correta na lista
+    nome = nome.lower().replace(" ","")
+
+    # Resolvendo o exercício 1 dos Algoritmos
+    encontrar_numero_por_nome(lista_nomes_telefone, nome)
